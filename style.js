d37f1dc9f025fd6a6d06ad4bf2cc1eddb1dc99c0
Alex <Like2968@gmail.com>
2025-05-29 01:45:57 +0800

    1-1 initialize first index.html version / basic structure

---
+++ style.js
@@ -0,0 +1,18 @@
+// 動態切換封面和主頁
+function enterMainSite() {
+    document.getElementById("welcomePage").classList.add("d-none");
+    document.getElementById("mainSite").classList.add("active");
+
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+
+// 動態調整當視窗滑動時navbar的透明度
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar-custom");
+    if (window.scrollY > 50) {
+        navbar.style.background = "rgba(0,0,0,0.95)";
+    } else {
+        navbar.style.background = "rgba(0,0,0,0.7)";
+    }
+});
