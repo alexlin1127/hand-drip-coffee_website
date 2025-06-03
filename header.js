858e98bd97e0b94f11566b9f52d2017d61cddd77
Alex <Like2968@gmail.com>
2025-06-03 22:33:17 +0800

    2-7 other-revise

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 const headerHTML = `
-    <div class="container">
+    <div class="container-fluid w-100 px-5">
         <a class="navbar-brand d-flex align-items-center" href="index.html">
-            <div class="logo-circle d-flex justify-content-center align-items-center me-2">
+            <div class="logo-circle d-flex justify-content-center align-items-center me-3">
                 <i class="bi bi-cup-hot"></i>
             </div>
             dement
@@ -14,28 +14,28 @@ const headerHTML = `
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item px-1">
-                    <a class="nav-link" id="homePage" href="index.html" target="_self">首頁</a>
+                    <a class="nav-link header-homepage" id="homePage" href="index.html" target="_self">首頁</a>
                 </li>
                 <li class="nav-item px-1">
-                    <a class="nav-link" href="news.html" target="_self">最新消息</a>
+                    <a class="nav-link header-news" href="news.html" target="_self">最新消息</a>
                 </li>
                 <li class="nav-item px-1">
-                    <a class="nav-link" href="info.html" target="_self">關於我們</a>
+                    <a class="nav-link header-info" href="info.html" target="_self">關於我們</a>
                 </li>
                 <li class="nav-item px-1">
-                    <a class="nav-link" href="product.html" target="_self">精選商品</a>
+                    <a class="nav-link header-product" href="product.html" target="_self">精選商品</a>
                 </li>
                 <li class="nav-item px-1">
-                    <a class="nav-link" href="contact.html" target="_self">聯絡資訊</a>
+                    <a class="nav-link header-contact" id="contact-info" href="#" target="">聯絡資訊</a>
                 </li>
             </ul>
         </div>
-        <a class="member-icon" href="member.html" target="_blank">
-            <i class="bi bi-person-circle"></i>
-        </a>
-        <a class="cart-icon" href="cart.html" target="_blank">
+        <a class="cart-icon position-relative" href="cart.html" target="_self" title="購物車">
             <i class="bi bi-cart3"></i>
+            <span id="cart-badge" class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style="display:none; font-size:0.7rem;">0</span>
         </a>
+
+        
     </div>
     `;
     
