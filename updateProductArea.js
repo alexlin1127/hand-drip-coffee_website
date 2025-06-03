372a9ebcb5fcf431a2997e73cca064e43d7f2b30
Alex <Like2968@gmail.com>
2025-06-03 22:30:54 +0800

    2-4 revise product price

--- updateProductArea.js
+++ updateProductArea.js
@@ -22,7 +22,7 @@ const desktop_productHTML = `
                                         <h5 class="card-title">安地斯晨霧</h5>
                                         <h5 class="card-title pb-2">Andes Mist</h5>
                                         <p class="card-text">口感柔和、酸甜均衡，帶有柑橘與紅糖的尾韻，如晨霧中甦醒的高原風情</p>
-                                        <div class="price-tag">NT$ 120</div>
+                                        <div class="price-tag">NT$ 360</div>
                                     </div>
                                 </div>
                             </div>
@@ -37,7 +37,7 @@ const desktop_productHTML = `
                                         <h5 class="card-title">肯亞黃昏</h5>
                                         <h5 class="card-title pb-2">Kenyan Dusk</h5>
                                         <p class="card-text">濃郁莓果香氣與葡萄酒般的質感，帶來層次分明的酸度與甜美的結尾</p>
-                                        <div class="price-tag">NT$ 150</div>
+                                        <div class="price-tag">NT$ 420</div>
                                     </div>
                                 </div>
                             </div>
@@ -52,7 +52,7 @@ const desktop_productHTML = `
                                         <h5 class="card-title">衣索比亞花開</h5>
                                         <h5 class="card-title pb-2">Ethiopian Blossom</h5>
                                         <p class="card-text">清新茉莉與柑橘香氣，如花園中綻放的晨光，適合追求輕盈風味的你</p>
-                                        <div class="price-tag">NT$ 200</div>
+                                        <div class="price-tag">NT$ 400</div>
                                     </div>
                                 </div>
                             </div>
@@ -67,7 +67,7 @@ const desktop_productHTML = `
                                         <h5 class="card-title">巴西暖陽</h5>
                                         <h5 class="card-title pb-2">Brazilian Warmth</h5>
                                         <p class="card-text">厚實的榛果香與黑巧克力尾韻，為午後帶來陽光般的溫暖擁抱</p>
-                                        <div class="price-tag">NT$ 200</div>
+                                        <div class="price-tag">NT$ 340</div>
                                     </div>
                                 </div>
                             </div>
@@ -85,7 +85,7 @@ const desktop_productHTML = `
                                         <h5 class="card-title">瓜地馬拉回聲</h5>
                                         <h5 class="card-title pb-2">Guatemalan Echo</h5>
                                         <p class="card-text">滑順口感中帶有焦糖與香料氣息，如山谷中迴盪的細語</p>
-                                        <div class="price-tag">NT$ 180</div>
+                                        <div class="price-tag">NT$ 380</div>
                                     </div>
                                 </div>
                             </div>
@@ -100,7 +100,7 @@ const desktop_productHTML = `
                                         <h5 class="card-title">耶加雪菲暮光</h5>
                                         <h5 class="card-title pb-2">Yirgacheffe Twilight</h5>
                                         <p class="card-text">散發花香與柑橘風味，搭配淡雅果酸，適合靜夜中細品回味</p>
-                                        <div class="price-tag">NT$ 160</div>
+                                        <div class="price-tag">NT$ 400</div>
                                     </div>
                                 </div>
                             </div>
@@ -115,7 +115,7 @@ const desktop_productHTML = `
                                         <h5 class="card-title">巴拿馬微風</h5>
                                         <h5 class="card-title pb-2">Panama Breeze</h5>
                                         <p class="card-text">帶有熱帶水果與蜂蜜香氣，口感細緻滑順，如微風拂面</p>
-                                        <div class="price-tag">NT$ 280</div>
+                                        <div class="price-tag">NT$ 450</div>
                                     </div>
                                 </div>
                             </div>
@@ -130,7 +130,7 @@ const desktop_productHTML = `
                                         <h5 class="card-title">蘇門答臘深林</h5>
                                         <h5 class="card-title pb-2">Sumatran Depth</h5>
                                         <p class="card-text">濃郁泥土與香草氣息，展現熱帶雨林的野性魅力與深邃風情</p>
-                                        <div class="price-tag">NT$ 280</div>
+                                        <div class="price-tag">NT$ 360</div>
                                     </div>
                                 </div>
                             </div>
